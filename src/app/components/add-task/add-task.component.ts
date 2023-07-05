b589a82d46a62f29a857ad1708cda4929ad81005
akashag05 <124151772+akashag05@users.noreply.github.com>
2023-07-05 16:59:34 +0530

    Task tracker completed with routing

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Task } from '../../Task';
-
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
@@ -11,6 +12,13 @@ export class AddTaskComponent {
   text: string | undefined;
   day: any;
   reminder: boolean = false;
+  showAddTask!: boolean;
+  subscription!: Subscription;
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
 
   onSubmit() {
     if (!this.text) {
--- src/app/services/ui.service.ts
+++ src/app/services/ui.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
-
+import { Observable, Subject } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UiService {
+  private showAddTask: boolean = false;
+  private subject = new Subject<any>();
+  constructor() {}
 
-  constructor() { }
+  toggleAddTask() {
+    this.showAddTask = !this.showAddTask;
+    this.subject.next(this.showAddTask);
+  }
+
+  onToggle() {
+    return this.subject.asObservable();
+  }
 }
